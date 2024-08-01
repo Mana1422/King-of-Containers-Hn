955de231cdb2c00ab2ba0164e14d30f3f1d91cff
Mana1422 <manasesvillalobos80@gmail.com>
2024-08-01 11:03:19 -0700

    Chage the animation in the portfolio page to show earlier.

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -17,5 +17,25 @@ const observer = new IntersectionObserver((entries) => {
 }, options);
 
 
+// Observer for the gallery
+const options2 = {
+    threshold: window.innerWidth <= 900? 0.1 :0.5 // the porcentage of visibilty to start the animation
+}
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        } 
+        // else {
+        //     entry.target.classList.remove('show');
+        // }
+    });
+}, options2);
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenGallery = document.querySelectorAll('.hiddenG');
+hiddenGallery.forEach(el => observer2.observe(el));
